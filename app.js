fa32c7758598cb0a678f64f12c7372e1bf73821b
amitlzkpa <amit.lzkpa@gmail.com>
2024-05-18 21:54:57 +0530

    Set up IP project

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 require("dotenv").config();
 const express = require("express");
+const IPGeolocationAPI = require("ip-geolocation-api-javascript-sdk");
+const GeolocationParams = require("ip-geolocation-api-javascript-sdk/GeolocationParams.js");
 
 const PORT = process.env.PORT || 3000;
 
+const apiKey = process.env.IP_GEOLOCATION_API_KEY;
+const ipgeolocationApi = new IPGeolocationAPI(apiKey);
+
 const app = express();
+app.set("trust proxy", true);
 
 app.get("/", function (req, res) {
-  res.send("Hello World!");
+  const ip =
+    req.headers["x-forwarded-for"] || req.ip || req.socket.remoteAddress;
+
+  console.log("-------------------");
+  console.log(ip);
+  console.log("-------------------");
+
+  const geolocationParams = new GeolocationParams();
+  geolocationParams.setIPAddress(ip);
+
+  ipgeolocationApi.getGeolocation((geolocation) => {
+    return res.json(geolocation);
+  }, geolocationParams);
 });
 
 app.listen(PORT, function () {
